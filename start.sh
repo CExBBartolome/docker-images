7bff71cdeedce80db288ae6a703cb99605d2424c
CExBBartolome <bbartolome@i-sight.com>
2017-03-27 12:15:58 -0400

    fix: redo group creation

--- start.sh
+++ start.sh
@@ -3,10 +3,20 @@
 # From https://github.com/SvenDowideit/docs-automation/blob/master/jenkins/setup-docker-and-start-jenkins.sh
 set -e
 
-DOCKER_GROUP=$(ls -al /var/run/docker.sock  | awk '{print $4}')
+JUSER="jenkins"
+DOCKER_GROUP="dockergrp"
+
+DOCKER_GID=$(ls -aln /var/run/docker.sock  | awk '{print $4}')
+
+if ! getent group $DOCKER_GID; then
+  echo creating $DOCKER_GROUP group $DOCKER_GID
+  addgroup --gid $DOCKER_GID $DOCKER_GROUP
+else
+  DOCKER_GROUP=$(getent group $DOCKER_GID | cut -f 1 -d :)
+fi
+
 if ! id -nG "$JUSER" | grep -qw "$DOCKER_GROUP"; then
   adduser $JUSER $DOCKER_GROUP
 fi
 
 /usr/sbin/sshd -D
-
